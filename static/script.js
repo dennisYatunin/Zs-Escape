809c4e82bcf759520a9ca43f88f79dc48bc3ad0f
dennisYatunin <dyatun@gmail.com>
2016-03-08 01:36:21 -0500

    Made a bike svg; fixed stuff

--- static/script.js
+++ static/script.js
@@ -1,32 +1,9 @@
+/*--------------------- Important Variable and Constants ---------------------*/
+
 var platformAngle = 0;
 
-const PLATFORM_CRX = 500, PLATFORM_CRY = 9800,
+const PLATFORM_CRX = 500, PLATFORM_CRY = 9800;
 // x- and y-coordinates of platform's center of rotation
-PLATFORM_CX = 500, PLATFORM_CY = 9770.00308054,
-// x- and y-coordinates of platform's center when platformAngle = 0
-PLATFORM_H = 46.1077006684, PLATFORM_L = 300;
-// half of platform's height and length
-
-var dx, dy;
-function circlePlatformColliding(cx, cy, r) {
-	cx = cos(-platformAngle) * (cx - PLATFORM_CRX) -
-	sin(-platformAngle) * (cy - PLATFORM_CRY) + PLATFORM_CRX;
-	cy = sin(-platformAngle) * (cx - PLATFORM_CRX) +
-	cos(-platformAngle) * (cy - PLATFORM_CRY) + PLATFORM_CRY;
-
-	dx = Math.abs(cx - PLATFORM_CX);
-	dy = Math.abs(cy - PLATFORM_CY);
-
-	if (dx > PLATFORM_L + r) { return false; }
-	if (dy > PLATFORM_H + r) { return false; }
-
-	if (dx <= PLATFORM_L) { return true; }
-	if (dy <= PLATFORM_H) { return true; }
-
-	dx -= PLATFORM_L;
-	dy -= PLATFORM_H;
-	return dx * dx + dy * dy <= r * r;
-}
 
 /*----------------------------- Platform Control -----------------------------*/
 
@@ -68,7 +45,8 @@ function drop_platform_mouse(evt) {
 	document.onmousemove = null;
 	document.onmouseup = null;
 
-	svg.style.cursor = 'url(\'static/hand1.cur\'), auto';
+	svg.style.cursor = 'url(\'static/hand1.cur\'),auto'; // open palm
+	platform.style.cursor = 'url(\'static/hand2.cur\'),auto'; // half-open palm
 }
 
 function drop_platform_touch(evt) {
@@ -85,7 +63,8 @@ platform.addEventListener('mousedown', function (evt) {
 	document.onmousemove = drag_platform_mouse;
 	document.onmouseup = drop_platform_mouse;
 
-	svg.style.cursor = 'url(\'static/hand3.cur\'), auto';
+	svg.style.cursor = 'url(\'static/hand3.cur\'),auto'; // closed palm
+	platform.style.cursor = 'url(\'static/hand3.cur\'),auto'; // closed palm
 });
 
 platform.addEventListener('touchstart', function (evt) {
@@ -98,11 +77,36 @@ platform.addEventListener('touchstart', function (evt) {
 	document.ontouchend = drop_platform_touch;
 });
 
-/*----------------------------- Zamansky Control -----------------------------*/
+/*--------------------------------- Z Motion ---------------------------------*/
+
+const PLATFORM_CX = 500, PLATFORM_CY = 9770.00308054,
+// x- and y-coordinates of platform's center when platformAngle = 0
+PLATFORM_H = 46.1077006684, PLATFORM_L = 300;
+// half of platform's height and length
+
+var dx, dy;
+function circlePlatformColliding(cx, cy, r) {
+	cx = cos(-platformAngle) * (cx - PLATFORM_CRX) -
+	sin(-platformAngle) * (cy - PLATFORM_CRY) + PLATFORM_CRX;
+	cy = sin(-platformAngle) * (cx - PLATFORM_CRX) +
+	cos(-platformAngle) * (cy - PLATFORM_CRY) + PLATFORM_CRY;
+
+	dx = Math.abs(cx - PLATFORM_CX);
+	dy = Math.abs(cy - PLATFORM_CY);
+
+	if (dx > PLATFORM_L + r) { return false; }
+	if (dy > PLATFORM_H + r) { return false; }
+
+	if (dx <= PLATFORM_L) { return true; }
+	if (dy <= PLATFORM_H) { return true; }
+
+	dx -= PLATFORM_L;
+	dy -= PLATFORM_H;
+	return dx * dx + dy * dy <= r * r;
+}
 
 const ROLLING_FRICTION_COEF = 0.002
 // typical rolling friction coefficient for a bicycle tire on concrete (unitless)
 // http://www.engineeringtoolbox.com/rolling-friction-resistance-d_1303.html
 
 var Z = {xPos: 0, yPos: 0, xVel: 0, yVel: 0, bikeAngle: 0, bikeAngleVel: 0, tireAngle: 0, curPower: 100};
-
