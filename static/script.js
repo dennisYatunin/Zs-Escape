450d741bf35312e8928e1bda05eb96b80ede3ca3
dennisYatunin <dyatun@gmail.com>
2016-03-09 01:32:03 -0500

    Did some stuff

--- static/script.js
+++ static/script.js
@@ -1,14 +1,34 @@
 /*--------------------- Important Variable and Constants ---------------------*/
 
-var platformAngle = 0;
+var platform = document.getElementById('platform'),
+platformAngle = 0;
 
-const PLATFORM_CRX = 500, PLATFORM_CRY = 9800;
+const PLATFORM_CRX = 500, PLATFORM_CRY = 9800,
 // x- and y-coordinates of platform's center of rotation
+PLATFORM_CX = 500, PLATFORM_CY = 9770.00308054,
+// x- and y-coordinates of platform's center when platformAngle = 0
+PLATFORM_L = 300, PLATFORM_H = 46.1077006684;
+// half of platform's length and height
+
+platform.setAttributeNS(null, 'x', PLATFORM_CRX);
+platform.setAttributeNS(null, 'y', PLATFORM_CRY);
+
+
+var Z = document.getElementById('Z');
+
+const Z_L1 = 47.3567056205,
+// horizontal distance between Z's leftmost point and his center of rotation
+Z_L2 = 52.6432943795,
+// horizontal distance between Z's rightmost point and his center of rotation
+Z_H = 60.6010016694;
+// vertical distance between Z's bottom-most point and his center of rotation
+
+Z.setAttributeNS(null, 'x', PLATFORM_CX - Z_L2 / 2 + Z_L1 / 2);
+Z.setAttributeNS(null, 'y', PLATFORM_CY - PLATFORM_H - Z_H);
 
 /*----------------------------- Platform Control -----------------------------*/
 
 var svg = document.getElementById('canvas'),
-platform = document.getElementById('platform'),
 pointGrabbed = svg.createSVGPoint(),
 pointDraggedTo = svg.createSVGPoint();
 
@@ -17,8 +37,12 @@ function change_platform_angle() {
 	pointDraggedTo.matrixTransform(svg.getScreenCTM().inverse());
 
 	platformAngle +=
-	Math.atan2(pointDraggedTo.y - PLATFORM_CRY, pointDraggedTo.x - PLATFORM_CRX)
-	- Math.atan2(pointGrabbed.y - PLATFORM_CRY, pointGrabbed.x - PLATFORM_CRX);
+	Math.atan2(
+		pointDraggedTo.y - PLATFORM_CRY, pointDraggedTo.x - PLATFORM_CRX
+		) -
+	Math.atan2(
+		pointGrabbed.y - PLATFORM_CRY, pointGrabbed.x - PLATFORM_CRX
+		);
 	platform.setAttributeNS(
 		null, 'transform',
 		'rotate(' + platformAngle * 180 / Math.PI + ' ' + PLATFORM_CRX + ' ' +
@@ -79,17 +103,28 @@ platform.addEventListener('touchstart', function (evt) {
 
 /*--------------------------------- Z Motion ---------------------------------*/
 
-const PLATFORM_CX = 500, PLATFORM_CY = 9770.00308054,
-// x- and y-coordinates of platform's center when platformAngle = 0
-PLATFORM_L = 300, PLATFORM_H = 46.1077006684;
-// half of platform's length and height
+var Zx = Z.getAttributeNS(null, 'x'), Zy = Z.getAttributeNS(null, 'y'),
+// x- and y-coordinates of Z's center of mass (center of rotation)
+Zxv = 0, Zyv = 0,
+// x- and y-components of the velocity of Z's center of mass
+Zangle = 0,
+// Z's current angle relative to the horizontal
+Zanglev = 0,
+// Z's current angular velocity
+Zpower = 0;
+// Z's current maximum power output
+
+const FRONT_WHEEL_DX = 31.3299944347, FRONT_WHEEL_DY = 39.4546466327,
+BACK_WHEEL_DX = -26.0434056758, BACK_WHEEL_DY = 39.4546466327,
+WHEEL_R = 20.0333889814;
 
 var dx, dy;
 function circlePlatformColliding(cx, cy, r) {
-	cx = cos(-platformAngle) * (cx - PLATFORM_CRX) -
-	sin(-platformAngle) * (cy - PLATFORM_CRY) + PLATFORM_CRX;
-	cy = sin(-platformAngle) * (cx - PLATFORM_CRX) +
-	cos(-platformAngle) * (cy - PLATFORM_CRY) + PLATFORM_CRY;
+	dx = cx - PLATFORM_CRX;
+	dy = cy - PLATFORM_CRY;
+
+	cx = cos(-platformAngle) * dx - sin(-platformAngle) * dy + PLATFORM_CRX;
+	cy = sin(-platformAngle) * dx + cos(-platformAngle) * dy + PLATFORM_CRY;
 
 	dx = Math.abs(cx - PLATFORM_CX);
 	dy = Math.abs(cy - PLATFORM_CY);
@@ -103,6 +138,16 @@ function circlePlatformColliding(cx, cy, r) {
 	dx -= PLATFORM_L;
 	dy -= PLATFORM_H;
 	return dx * dx + dy * dy <= r * r;
+}
+
+var Ffront, Fback, cx, cy, edgeColliding;
+function platformPushZ() {
+	Ffront = 0;
+	cx = cos(Zangle) * FRONT_WHEEL_DX - sin(Zangle) * FRONT_WHEEL_DY + Zx;
+	cy = sin(Zangle) * FRONT_WHEEL_DX + cos(Zangle) * FRONT_WHEEL_DY + Zy;
+	if (circlePlatformColliding(cx, cy, WHEEL_R)) {
+
+	}
 }
 
 const ROLLING_FRICTION_COEF = 0.002
